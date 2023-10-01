62462fc33270b9a6375ea5cbf7953f1ee2f0525e
Gabriel Sartori Rangel <gabriel.sartorirangel@gmail.com>
2023-10-01 01:34:25 -0300

    Corrigir erros

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ def next_cell(cell):
 def is_full(board):
     return all(len(x) == 1 for row in board for x in row)
 
+def board_copy(board): return [[[x for x in cell] for cell in row] for row in board]
+
 def neighbors_list(board, row, column): #retorna uma lista com os vizinhos
     top_num = board[row-1][column] if row > 0 else 0
     right_num = board[row][column+1] if column != (size-1) else 0
@@ -84,7 +86,7 @@ def is_valid(board, size, region_size):
             for k in range(size-1):
                 if region[k] == region[k+1]:
                     return False
-        return True    
+    return True    
 
 def is_placement_valid(board, row, column, value, size, region_size, 
                        top_num=0, right_num=0, bottom_num=0, left_num=0,
@@ -149,49 +151,53 @@ def print_board(board, size):
         print()
     print("--"*size)
 
+def count_pops(top, right, bottom, left):
+    pop_front = 0
+    pop_back = 0
+
+    pop_front += top == '^'
+    pop_front += right == '>'
+    pop_front += bottom == 'v'
+    pop_front += left == '<'
+
+    pop_back += top == 'v'
+    pop_back += right == '<'
+    pop_back += bottom == '^'
+    pop_back += left == '>'
+
+    return pop_front, pop_back
+
 def pre_process(board, size, region_size):
-    copy = [[x for x in y] for y in board]
+    copy = [[[x for x in cell] for cell in row] for row in board]
     success = True
     total = 0
+    
+    # Remove borders for smaller or bigger neighbors
     for row in range(size):
         for column in range(size):
-            if len(copy[row][column]) == 1:
+            if len(copy[row][column]) != size:
                 continue
             top, right, bottom, left = sign_board[row][column]
-            if top == "v":
-                copy[row][column].pop()
-            if top == "^":
+            pop_front, pop_back = count_pops(top, right, bottom, left)
+            for _ in range(pop_front):
                 copy[row][column].pop(0)
-            if right == ">":
+            for _ in range(pop_back):
                 copy[row][column].pop()
-            if right == "<":
-                copy[row][column].pop(0)
-            if bottom == "v":
-                copy[row][column].pop()
-            if bottom == "^":
-                copy[row][column].pop(0)
-            if left == ">":
-                copy[row][column].pop()
-            if left == "<":
-                copy[row][column].pop(0)
 
     while success:
         total += 1
         success = False
-        for i in range(size):
-            for j in range(size):
-                if len(copy[i][j]) == 1:
+        for row in range(size):
+            for column in range(size):
+                if len(copy[row][column]) == 1:
                     continue
-                for k in copy[i][j]:
-                    row = i
-                    column = j
-                    top, right, bottom, left = sign_board[row][column]
-                    symbols = top, right, bottom, left
-                    
-                    neighbors_numbers = neighbors(board, row, column)
-                    if not is_placement_valid(copy, i, j, k, size, region_size, *neighbors_numbers, *symbols):
-                        copy[i][j].remove(k)
+                symbols = [x for x in sign_board[row][column]]
+                neighbors_numbers = neighbors(board, row, column)
+                for k in copy[row][column]:
+                    if not is_placement_valid(copy, row, column, k, size, region_size, *neighbors_numbers, *symbols):
+                        copy[row][column].remove(k)
                         success = True
+    print(total)
     return copy
 
 def solve(board, row, column, size, region_size, checks=0, nivel=0):
@@ -207,17 +213,17 @@ def solve(board, row, column, size, region_size, checks=0, nivel=0):
     top, right, bottom, left = sign_board[row][column]
     symbols = top, right, bottom, left
     
-    neighbors = neighbors_signs(board, row, column)
+    neighbors_ = neighbors(board, row, column)
 
-    if len(board[row][column]) == 1:
+    if len(board[row][column]) == 0:
         return False, checks
 
     # Try possibilities
     for i in board[row][column]:
-        copy = [[x for x in y] for y in board]
+        copy = [[[x for x in cell] for cell in row] for row in board]
         copy[row][column] = [i]
         checks += 1
-        if not is_placement_valid(copy, row, column, i, size, region_size, *neighbors, *symbols):
+        if not is_placement_valid(copy, row, column, i, size, region_size, *neighbors_, *symbols):
             continue
         if (result := solve(copy, next_row, next_column, size, region_size, checks, nivel=nivel+1))[0] != False:
             return result
@@ -230,15 +236,3 @@ def print_count_possibilities(board, size):
             print(len(board[i][j]), end=' ')
         print()
     print("--"*size)
-
-processed = pre_process(board, size, region_size)
-
-new_board, checks = solve(board, 0, 0, size, region_size)
-if new_board != False:
-    print_board(new_board, size)
-else:
-    print("No solution found.")
-
-print("Checks: ", checks)
-print_count_possibilities(processed, size)
-print_board(processed, size)
