3f0c699e7d682900248aacd7d0bc0dc127de9a5d
Gabriel Sartori Rangel <gabriel.sartorirangel@gmail.com>
2023-12-04 01:03:59 -0300

    Adaptando código do github

--- prolog/github.pl
+++ prolog/github.pl
@@ -3,17 +3,16 @@
 
 comparadoresPorCelula(C) :-
     C = [
-    [['/', '+', '+', '/'], ['/', '+', '-', '-'], ['/', '/', '-', '-'], ['/', '-', '+', '/'], ['/', '+', '+', '+'], ['/', '/', '+', '-'], ['/', '-', '-', '/'], ['/', '+', '+', '+'], ['/', '/', '-', '-']],
-    [['-', '-', '-', '/'], ['+', '-', '+', '+'], ['+', '/', '+', '+'], ['-', '-', '-', '/'], ['-', '+', '-', '+'], ['-', '/', '-', '-'], ['+', '+', '+', '/'], ['-', '-', '+', '-'], ['+', '/', '+', '+']],
-    [['+', '+', '/', '/'], ['-', '+', '/', '-'], ['-', '/', '/', '-'], ['+', '+', '/', '/'], ['+', '-', '/', '-'], ['+', '/', '/', '+'], ['-', '+', '/', '/'], ['-', '-', '/', '-'], ['-', '/', '/', '+']],
-    [['/', '-', '-', '/'], ['/', '-', '-', '+'], ['/', '/', '-', '+'], ['/', '+', '+', '/'], ['/', '-', '+', '-'], ['/', '/', '+', '+'], ['/', '+', '+', '/'], ['/', '+', '-', '-'], ['/', '/', '+', '-']],
-    [['+', '-', '+', '/'], ['+', '-', '+', '+'], ['+', '/', '+', '+'], ['-', '-', '+', '/'], ['-', '-', '+', '+'], ['-', '/', '+', '+'], ['-', '-', '-', '/'], ['+', '+', '-', '+'], ['-', '/', '-', '-']],
-    [['-', '-', '/', '/'], ['-', '+', '/', '+'], ['-', '/', '/', '-'], ['-', '-', '/', '/'], ['-', '-', '/', '+'], ['-', '/', '/', '+'], ['+', '-', '/', '/'], ['+', '+', '/', '+'], ['+', '/', '/', '-']],
-    [['/', '+', '+', '/'], ['/', '-', '+', '-'], ['/', '/', '+', '+'], ['/', '+', '+', '/'], ['/', '-', '-', '-'], ['/', '/', '-', '+'], ['/', '+', '+', '/'], ['/', '+', '+', '-'], ['/', '/', '-', '-']],
-    [['-', '+', '-', '/'], ['-', '-', '-', '-'], ['-', '/', '-', '+'], ['-', '-', '+', '/'], ['+', '-', '+', '+'], ['+', '/', '+', '+'], ['-', '+', '+', '/'], ['-', '-', '+', '-'], ['+', '/', '-', '+']],
-    [['+', '-', '/', '/'], ['+', '+', '/', '+'], ['+', '/', '/', '-'], ['-', '+', '/', '/'], ['-', '+', '/', '-'], ['-', '/', '/', '-'], ['-', '+', '/', '/'], ['-', '-', '/', '-'], ['+', '/', '/', '+']]
+    [['.', '>', '>', '.'], ['.', '>', '<', '<'], ['.', '.', '<', '<'], ['.', '<', '>', '.'], ['.', '>', '>', '>'], ['.', '.', '>', '<'], ['.', '<', '<', '.'], ['.', '>', '>', '>'], ['.', '.', '<', '<']],
+    [['<', '<', '<', '.'], ['>', '<', '>', '>'], ['>', '.', '>', '>'], ['<', '<', '<', '.'], ['<', '>', '<', '>'], ['<', '.', '<', '<'], ['>', '>', '>', '.'], ['<', '<', '>', '<'], ['>', '.', '>', '>']],
+    [['>', '>', '.', '.'], ['<', '>', '.', '<'], ['<', '.', '.', '<'], ['>', '>', '.', '.'], ['>', '<', '.', '<'], ['>', '.', '.', '>'], ['<', '>', '.', '.'], ['<', '<', '.', '<'], ['<', '.', '.', '>']],
+    [['.', '<', '<', '.'], ['.', '<', '<', '>'], ['.', '.', '<', '>'], ['.', '>', '>', '.'], ['.', '<', '>', '<'], ['.', '.', '>', '>'], ['.', '>', '>', '.'], ['.', '>', '<', '<'], ['.', '.', '>', '<']],
+    [['>', '<', '>', '.'], ['>', '<', '>', '>'], ['>', '.', '>', '>'], ['<', '<', '>', '.'], ['<', '<', '>', '>'], ['<', '.', '>', '>'], ['<', '<', '<', '.'], ['>', '>', '<', '>'], ['<', '.', '<', '<']],
+    [['<', '<', '.', '.'], ['<', '>', '.', '>'], ['<', '.', '.', '<'], ['<', '<', '.', '.'], ['<', '<', '.', '>'], ['<', '.', '.', '>'], ['>', '<', '.', '.'], ['>', '>', '.', '>'], ['>', '.', '.', '<']],
+    [['.', '>', '>', '.'], ['.', '<', '>', '<'], ['.', '.', '>', '>'], ['.', '>', '>', '.'], ['.', '<', '<', '<'], ['.', '.', '<', '>'], ['.', '>', '>', '.'], ['.', '>', '>', '<'], ['.', '.', '<', '<']],
+    [['<', '>', '<', '.'], ['<', '<', '<', '<'], ['<', '.', '<', '>'], ['<', '<', '>', '.'], ['>', '<', '>', '>'], ['>', '.', '>', '>'], ['<', '>', '>', '.'], ['<', '<', '>', '<'], ['>', '.', '<', '>']],
+    [['>', '<', '.', '.'], ['>', '>', '.', '>'], ['>', '.', '.', '<'], ['<', '>', '.', '.'], ['<', '>', '.', '<'], ['<', '.', '.', '<'], ['<', '>', '.', '.'], ['<', '<', '.', '<'], ['>', '.', '.', '>']]
     ].
-
 
 %ACHAR O I-ÉSIMO DA LISTA
 %O 0-ésimo item da lista é a cabeça da lista, se ela tiver um ou mais itens.
@@ -30,14 +29,16 @@ nthMatriz(Linha, Coluna, Lista, X) :- nthLista(Linha, Lista, R), nthLista(Coluna
 
 
 %COMPARACOES ENTRE CELULAS
-%Se o comparador for '/' não faz nada
-compara(_, _, _, _, '/').
-compara(Num1, Tabuleiro, Linha, Coluna, '+') :- nthMatriz(Linha, Coluna, Tabuleiro, Num2), Num1 #> Num2.
-compara(Num1, Tabuleiro, Linha, Coluna, '-') :- nthMatriz(Linha, Coluna, Tabuleiro, Num2), Num1 #< Num2.
+%Se o comparador for '.' não faz nada
+compare_sign(_, _, _, _, '.').
+compare_sign(Num1, Board, Linha, Coluna, '>') :- nthMatriz(Linha, Coluna, Board, Num2), Num1 #> Num2.
+compare_sign(Num1, Board, Linha, Coluna, '<') :- nthMatriz(Linha, Coluna, Board, Num2), Num1 #< Num2.
 
+% Checa se todos os vizinhos estão ok com os sinais
+% sign_valid(Tabuleiro, Linha, Coluna)
 
 %Verifica se o valor é válido para a célula na linha X, coluna Y
-define_numero_valido(Num, Linha, Coluna, Tabuleiro, Comparacoes) :- 
+is_placement_valid(Num, Linha, Coluna, Board, Comparacoes) :- 
     %Comparadores da célula na linha X, coluna Y
     %Busca na matriz de comparadores (Comparadores)
     %Salva os comparadores em ComparadoresCelula
@@ -57,56 +58,61 @@ define_numero_valido(Num, Linha, Coluna, Tabuleiro, Comparacoes) :-
     Bottom is Linha + 1,
 
     %Define as comparações por direção
-    compara(Num, Tabuleiro, Top, Coluna, ComparaAcima),
-    compara(Num, Tabuleiro, Linha, Right, ComparaDireita),
-    compara(Num, Tabuleiro, Bottom, Coluna, ComparaAbaixo),
-    compara(Num, Tabuleiro, Linha, Left, ComparaEsquerda).
+    compare_sign(Num, Board, Top, Coluna, ComparaAcima),
+    compare_sign(Num, Board, Linha, Right, ComparaDireita),
+    compare_sign(Num, Board, Bottom, Coluna, ComparaAbaixo),
+    compare_sign(Num, Board, Linha, Left, ComparaEsquerda).
 
 
 %Percorre todas posições da matriz e define as comparações referentes a cada posição.
-regras(_, _, 9, _).
-regras(Tabuleiro, Comparadores, Linha, Coluna) :- 
-    nthMatriz(Linha, Coluna, Tabuleiro, Num),
-    define_numero_valido(Num, Linha, Coluna, Tabuleiro, Comparadores),
+solve(_, _, 9, _).
+solve(Board, Sign_board, Linha, Coluna) :- 
+    nthMatriz(Linha, Coluna, Board, Num),
+    is_placement_valid(Num, Linha, Coluna, Board, Sign_board),
     N is Coluna + 1,
-    regras(Tabuleiro, Comparadores, Linha, N).
+    solve(Board, Sign_board, Linha, N).
 %Caso esteja na última coluna
-regras(Tabuleiro, Comparadores, Linha, 9) :- N is Linha + 1, regras(Tabuleiro, Comparadores, N, 0).
-
+solve(Board, Sign_board, Linha, 9) :- N is Linha + 1, solve(Board, Sign_board, N, 0).
 
 %Regras do Jogo
-vergleich(Tabuleiro, Comparadores) :-
+vergleich(Board, Sign_board, Size) :-
     %9 linhas
-    length(Tabuleiro, 9),
+    length(Board, Size),
 
     %9 colunas
-    maplist(same_length(Tabuleiro), Tabuleiro),
+    maplist(same_length(Board), Board),
 
     %Concatena todos os números do tabuleiro em NumerosTotal
-    append(Tabuleiro, TodosOsNumeros),
+    append(Board, TodosOsNumeros),
 
     %O tabuleiro tem apenas números entre 1 e 9
     %ins(Vars, Domain): Vars-Lista de variáveis; Domain:Valores que Vars pode assumir
-    TodosOsNumeros ins 1..9, % O tabuleiro tem apenas números entre 1 e 9
+    TodosOsNumeros ins 1..Size, % O tabuleiro tem apenas números entre 1 e 9
 
     %O tabuleiro segue a regra do Tabuleiro vergleich
-    regras(Tabuleiro, Comparadores, 0, 0), % O tabuleiro segue a regra do Tabuleiro vergleichs
+    solve(Board, Sign_board, 0, 0), % O tabuleiro segue a regra do Tabuleiro vergleichs
 
     %Sem repetição de valores por linha
-    maplist(all_distinct, Tabuleiro), % Todas as linhas possuem números entre 1 e 9 sem repetições
+    maplist(all_distinct, Board), % Todas as linhas possuem números entre 1 e 9 sem repetições
 
     %Inverte as linhas e colunas
-    transpose(Tabuleiro, Columns), % Inverte as linhas e colunas
+    transpose(Board, Columns), % Inverte as linhas e colunas
 
     %Sem repetição de valores por coluna (agora linha)
     maplist(all_distinct, Columns),  % Todas as colunas possuem números entre 1 e 9 sem repetições
 
     %A partir das linhas nomeadas, divide em 3 regioes de 3 linhas cada
     %Sem repetição de valores por regiao
-    Tabuleiro = [Linha1, Linha2, Linha3, Linha4, Linha5, Linha6, Linha7, Linha8, Linha9], % Nomeia todas as linhas da Matriz
+    Board = [Linha1, Linha2, Linha3, Linha4, Linha5, Linha6, Linha7, Linha8, Linha9], % Nomeia todas as linhas da Matriz
+
+    % region_size_x is integer(sqrt(size)),
+    % region_size_y is size // region_size_x,
 
     %A partir das linhas nomeadas, divide em 3 regioes de 3 linhas cada
     %Sem repetição de valores por regiao
+    % regiao(Linha1, Linha2, Linha3, region_size_x),
+    % regiao(Linha4, Linha5, Linha6, region_size_x),
+    % regiao(Linha7, Linha8, Linha9, region_size_x),
     regiao(Linha1, Linha2, Linha3),
     regiao(Linha4, Linha5, Linha6),
     regiao(Linha7, Linha8, Linha9).
@@ -121,8 +127,18 @@ regiao([N1,N2,N3|Ns1], [N4,N5,N6|Ns2], [N7,N8,N9|Ns3]) :-
     all_distinct([N1,N2,N3,N4,N5,N6,N7,N8,N9]),
     regiao(Ns1, Ns2, Ns3).
 
+% regiao([], [], [], _).
+% regiao([N1,N2,N3|Ns1], [N4,N5,N6|Ns2], [N7,N8,N9|Ns3], Linhas) :-
+%     length([N1,N2,N3,N4,N5,N6,N7,N8,N9], Comprimento),
+%     all_distinct([N1,N2,N3,N4,N5,N6,N7,N8,N9]),
+%     LinhasRestantes is Linhas - 1,
+%     regiao(Ns1, Ns2, Ns3, LinhasRestantes),
+%     Comprimento =:= size.
 
-main :- comparadoresPorCelula(Comparadores),
-    vergleich(P, Comparadores),
+size(9).
+
+main :- comparadoresPorCelula(Sign_board),
+    size(X),    
+    vergleich(P, Sign_board, X),
     maplist(label, P), maplist(portray_clause, P),
     halt.
